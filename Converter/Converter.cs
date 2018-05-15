cace9413cc43becee034389ffd69127232a9f79f
Funnybanny <vazzzeg001@gmail.com>
2018-05-15 19:45:27 +0200

    Converter Logic

--- Converter/Converter.cs
+++ Converter/Converter.cs
@@ -1,10 +1,62 @@
-﻿using System;
+﻿using BE;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Converter
 {
     class Converter
     {
+        string path = @"C:\Users\KristofHorvath\Desktop\Folder";
+
+        public void Convert()
+        {
+            ServiceGateway sg = new ServiceGateway();
+            sg.postMany(ReadFiles());
+        }
+
+        private string[]  GetFiles()
+        {
+            string[] files = System.IO.Directory.GetFiles(path, "*.txt");
+
+            return files;
+        }
+
+        private List<Customer> ReadFiles()
+        {
+            List<Customer> lst = new List<Customer>();
+            string[] files = GetFiles();
+            string line;
+            for (int i = 0; i < files.Length; i++)
+            {
+                int counter = 0;
+                Customer c = new Customer();
+                StreamReader file = new StreamReader(files[i]);
+                while ((line = file.ReadLine()) != null)
+                {
+                    string l = line.Substring(11);
+                    if (counter == 0)
+                    {
+                        c.Name = l;
+                    }
+                    if (counter == 1)
+                    {
+                        c.BirthDate = DateTime.ParseExact(l, "d", null);
+                    }
+                    if (counter == 2)
+                    {
+                        c.Address = l;
+                    }
+                    if (counter == 3)
+                    {
+                        c.PhoneNumber = Int32.Parse(l);
+                    }
+                    counter++;
+                }
+                lst.Add(c);
+            }
+            return lst;
+        }
     }
 }
--- Converter/ServiceGateway.cs
+++ Converter/ServiceGateway.cs
@@ -11,16 +11,6 @@ namespace Converter
     class ServiceGateway
     {
         string connection = "http://localhost:64943/api/customer/";
-        public void postOne(Customer c)
-        {
-            RestClient client = new RestClient();
-            // You may need to change the port number in the BaseUrl below
-            // before you can run the request.
-            client.BaseUrl = new Uri(connection);
-            var request = new RestRequest(Method.POST);
-            request.AddParameter("application/json; charset=utf-8", JsonConvert.SerializeObject(c), ParameterType.RequestBody);
-            var response = client.Execute<Customer>(request);
-        }
 
         public void postMany(List<Customer> lst)
         {
